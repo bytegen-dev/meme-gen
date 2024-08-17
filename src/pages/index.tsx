ea125e10dc425df1f797525857528f4a0c364846
Isaac ⚡ <secondsonofadebayo@gmail.com>
2024-08-17 18:23:42 +0100

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,10 +12,10 @@ export default function Home() {
     <>
       <div className="min-h-screen bg-gray-100 page-el flex page-el flex-col items-center justify-center">
         <header className="p-4 text-white text-center">
-          <h1 className="text-4xl sax font-bold">MemeGen 💩</h1>
+          <h1 className="text-4xl sax font-bold">MemeGen 🙂</h1>
         </header>
         <main className="p-8 text-center mainres">
-          <p>Just a basic meme generator built in 60 minutes with NEXT.js and Typescript 🙂</p>
+          <p>Just a basic meme generator built in <b>60 minutes</b> with NEXT.js and Typescript</p>
           <Link href="/generate" className="mt-6 inline-block bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 primary-btn">
             Create a Meme
           </Link>
